e5902cb3fcded7452580466e756517f7c65b89b5
Ghadeer <ghaora@gmail.com>
2022-03-17 17:20:01 +0200

    dashboard+posts

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -71,20 +71,43 @@
                 </div>
             </div>
         </nav>
+        <div>
+        <div class="d-flex justify-content-center">
+            <ul class="list-unstyled mx-3 my-5">
+                <li class="mb-2">
+                    <a href="{{ route('admin.home') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
+                        <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
+                        </svg>  
+                        Dashboard
+                    </a>
+                </li>
+                <li class="mb-2">
+                    <a href="{{ route('admin.posts.index') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+                        <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+                        <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+                        </svg>
+                        Posts
+                    </a>
 
-        <main class="py-4">
-            
-            <div class="container">
-                <div class="row d-flex justify-content-center">
-                    <div class="col-10 d-flex justify-content-center flex-column">
-                        <h1 class="font-weight-bold text-uppercase text-danger m-3 text-center bg-info p-5">Posts</h1>
-                        <div>
-                            @yield('content')
+                </li>
+            </ul>
+                
+            <main class="py-4">        
+                <div class="container">
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-12 d-flex justify-content-center flex-column">
+                            <h1 class="font-weight-bold text-uppercase text-danger m-3 text-center bg-info p-5">Posts</h1> 
+                                <div>
+                                    @yield('content')
+                                </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </div>
+
     </div>
 </body>
 </html>
